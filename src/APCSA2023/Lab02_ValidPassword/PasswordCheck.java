fbf959f55efa2a910bc5d8833af1c6611d3f5a91
GW-Dev <grantewilliams51@gmail.com>
2022-09-29 08:50:09 -0500

    4

--- src/APCSA2023/Lab02_ValidPassword/PasswordCheck.java
+++ src/APCSA2023/Lab02_ValidPassword/PasswordCheck.java
@@ -10,10 +10,22 @@ class PasswordCheck
 
 	public PasswordCheck()
 	{
+		this.password = "mchammergohammer";
 	}
 
 	public void check()
 	{
 		Scanner keyboard = new Scanner(System.in);
+		String keys;
+		do {
+			System.out.print("Enter the password :: ");
+			keys = keyboard.next();
+			if (keys.equals(this.password)) {
+				System.out.println("VALID");
+			}
+			else {
+				System.out.println("INVALID");
+			}
+		} while(!keys.equals(this.password));
 	}
 }
