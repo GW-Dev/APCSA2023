80b760f0ab0f0833718b9e280da619cf3b91859b
GW-Dev <grantewilliams51@gmail.com>
2022-11-04 11:10:09 -0500

    26

--- src/APCSA2023/Unit_04/Lab06_TriangleWords/TriangleWord.java
+++ src/APCSA2023/Unit_04/Lab06_TriangleWords/TriangleWord.java
@@ -13,9 +13,52 @@ public class TriangleWord
 		triWord = "";
 	}
 
+	public void setWord(String ww) {
+		word = ww;
+	}
+
+	private String numSpaces(int nS) {
+		String toReturn = "";
+		for (int loopSpace = 0; loopSpace < nS-1; loopSpace++) {
+			toReturn += " ";
+		}
+		return toReturn;
+	}
+
+	public String getBackWords(String wordToReverse) {
+		word = wordToReverse;
+		char[] wordArray = wordToReverse.toCharArray();
+		char[] wordArray2 = wordToReverse.toCharArray();
+		for (int i = wordToReverse.length(); i > 0; i--) {
+			char transferArray = wordArray[word.length() - i];
+			wordArray2[i - 1] = transferArray;
+		}
+		return String.valueOf(wordArray2);
+	}
+
+	private void makeTriangle(String theWord) {
+		int lengthWordArr = theWord.length();
+		String[] triArray = new String[lengthWordArr];
+		for (int row = 0; row < theWord.length(); row++) {
+			if (row == 0) {
+				triArray[0] = this.numSpaces((lengthWordArr - row)) + word.charAt(row);
+			} else if ((row > 0) && (row < lengthWordArr-1)) {
+				triArray[row] = (this.numSpaces((lengthWordArr - row)) +
+						word.charAt(row) + this.numSpaces(((2*row)))
+						+ word.charAt(row));
+			} else if (row == lengthWordArr-1) {
+				triArray[row] = this.getBackWords(theWord) + theWord.substring(1, lengthWordArr - 1) + theWord.charAt(row);
+			}
+		}
+		for (int i = 0; i < lengthWordArr ; i++) {
+			triWord += triArray[i] + "\n";
+		}
+
+	}
+
 	public String toString()
 	{
-		String output="";
-		return output+"\n";
+		makeTriangle(word);
+		return triWord;
 	}
 }
--- src/APCSA2023/Unit_04/Lab06_TriangleWords/TriangleWordRunner.java
+++ src/APCSA2023/Unit_04/Lab06_TriangleWords/TriangleWordRunner.java
@@ -5,6 +5,13 @@
 public class TriangleWordRunner
 {
    public static void main(String args[])
-   {	
+   {
+       String[] tc = new String[] {"WOW", "BEN", "SAMMY", "WILE", "APLUSCOMPSCI"};
+       for (int e = 0; e < 5; e++) {
+           TriangleWord tw = new TriangleWord("");
+           tw.setWord(tc[e]);
+           System.out.println(tw);
+           System.out.println("\n");
+       }
 	}
 }
