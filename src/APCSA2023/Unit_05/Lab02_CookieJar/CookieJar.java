7d2769236af657f4960803dd62d43adb9fb17a5e
GW-Dev <grantewilliams51@gmail.com>
2022-11-07 09:02:41 -0600

    30

--- src/APCSA2023/Unit_05/Lab02_CookieJar/CookieJar.java
+++ src/APCSA2023/Unit_05/Lab02_CookieJar/CookieJar.java
@@ -1,7 +1,42 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
+class CookieJar {
+    private int capacity;
+    private int numCookies;
 
+    public CookieJar(int cap) {
+        capacity = cap;
+        numCookies = 0;
+    }
+
+    public int addStuff(int cookAdd) {
+        int leftOver = 0;
+        if (cookAdd > capacity-numCookies) {
+            leftOver = (numCookies + cookAdd - capacity);
+            numCookies += (capacity-numCookies) % cookAdd;
+            return leftOver;
+        }
+        else {
+            numCookies += cookAdd;
+            return leftOver;
+        }
+    }
+
+    public boolean isFull() {
+        if (numCookies >= capacity) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int spaceLeft() {
+        return (capacity-numCookies);
+    }
+
+}
 //design your CookieJar class
 //write all code for your CookieJar class here
 //test your CookieJar class with the CookieJarRunner
