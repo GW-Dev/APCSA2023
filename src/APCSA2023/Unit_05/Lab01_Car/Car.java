3bce4c9dc6da917c027a3b9add159929f3ae6d97
GW-Dev <grantewilliams51@gmail.com>
2022-11-07 08:27:30 -0600

    28

--- src/APCSA2023/Unit_05/Lab01_Car/Car.java
+++ src/APCSA2023/Unit_05/Lab01_Car/Car.java
@@ -1,7 +1,33 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
+class Car
+{
+    private int miles;
+    private int milesTilOilChange;
 
+    private int lastOil;
+
+    public Car(int mi, int miOil) {
+        miles = mi;
+        milesTilOilChange = miOil;
+        lastOil = 0;
+    }
+
+    public void addMiles(int toAdd) {
+        miles += toAdd;
+    }
+
+    public boolean timeForOilChange() {
+        if (miles-lastOil > milesTilOilChange) {
+            lastOil += miles;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
 //design your Car class
 //write all code for your Car class here
 //test your Car class with the CarRunner
