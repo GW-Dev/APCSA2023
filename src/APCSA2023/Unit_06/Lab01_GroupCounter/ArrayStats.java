2f1a545b0dcdcd5c3749ffb117dc5093b41d56a1
GW-Dev <grantewilliams51@gmail.com>
2022-12-07 08:46:07 -0600

    35

--- src/APCSA2023/Unit_06/Lab01_GroupCounter/ArrayStats.java
+++ src/APCSA2023/Unit_06/Lab01_GroupCounter/ArrayStats.java
@@ -8,27 +8,68 @@ import static java.lang.System.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArrayStats
-{
-	private int[] array;
-	
-	public ArrayStats(String list)
-	{
-		//put stuff in the array	
-	}	
-	
-	public void setArray(String list)
-	{
-		//put stuff in the array		
-	}	
-			
-	public int getNumGroupsOfSize(int size)
-	{
-		return -2938;
+
+public class ArrayStats {
+	private String[] array;
+
+	public ArrayStats(String list) {
+		//put stuff in the array
+		String input = list;
+		String modInput = "";
+		for (int i = 0; i < input.length()-1; i++) {
+			char transStep = input.charAt(i);
+			if (!(Character.isWhitespace(transStep))) {
+				modInput += transStep;
+			}
+			array = new String[modInput.length() + 1];
+			for (int index = 0; index < array.length-1; index++) {
+				array[index] = String.valueOf(modInput.charAt(index));
+			}
+		}
+		array[array.length-1] = input.substring(input.length()-1, input.length());
 	}
-	
-	public String toString()
-	{
-		return "return the array as a string";
+
+	public void setArray(String list) {
+		String input = list;
+		String modInput = "";
+		for (int i = 0; i < input.length()-1; i++) {
+			char transStep = input.charAt(i);
+			if (!(Character.isWhitespace(transStep))) {
+				modInput += transStep;
+			}
+			array = new String[modInput.length() + 1];
+			for (int index = 0; index < array.length-1; index++) {
+				array[index] = String.valueOf(modInput.charAt(index));
+			}
+		}
+		array[array.length-1] = input.substring(input.length()-1);
 	}
-}
+
+
+		public int getNumGroupsOfSize (int size)
+		{
+			int groupNum = 0;
+			String[] groupNumArr = new String[size];
+			String[] singleArr = new String[size];
+			for (int i = 0; i < array.length; i+=(size)) {
+				for (int j = 0; j < size; j++) {
+					groupNumArr[j] = array[i];
+					Arrays.fill(singleArr, groupNumArr[0]);
+					if ((groupNumArr[j] != null && singleArr[j] != null)) {
+						out.println("Single Array " + Arrays.toString(singleArr) + " Group Array " + Arrays.toString(groupNumArr));
+						if (Arrays.equals(groupNumArr,singleArr) && j % size == 0) {
+							out.println("CHOSEN: Single Array " + Arrays.toString(singleArr) + " Group Array " + Arrays.toString(groupNumArr));
+							groupNum++;
+						}
+					}
+
+				}
+			}
+			return groupNum;
+		}
+
+		public String toString()
+		{
+			return Arrays.toString(array);
+		}
+	}
