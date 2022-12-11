02ecf6c3179684ec06aafef5d22d01aeef634998
GW-Dev <grantewilliams51@gmail.com>
2022-12-11 17:21:38 -0600

    36

--- src/APCSA2023/Unit_07/Lab01_FactorTree/ArrayListFunHouse.java
+++ src/APCSA2023/Unit_07/Lab01_FactorTree/ArrayListFunHouse.java
@@ -5,17 +5,44 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.lang.Math;
+import java.util.Arrays;
 import java.util.Collections;
 import static java.lang.System.*;
 
 public class ArrayListFunHouse
 {
+
 	public static ArrayList<Integer> getListOfFactors(int number)
 	{
-		return null;
+		ArrayList<Integer> factorList = new ArrayList<>();
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0) {
+				factorList.add(i);
+			}
+		}
+		return factorList;
 	}
 	
-	public static void keepOnlyCompositeNumbers( List<Integer> nums )
-	{
+	public static void keepOnlyCompositeNumbers( List<Integer> nums ) {
+		List<Integer> compositeList = new ArrayList<>();
+		int lastFactor = nums.get(nums.size()-1);
+
+		for (int j = 2; j < Math.sqrt(lastFactor); j++) {
+			for (int k = 0; k < nums.size()-1; k++) {
+				if (nums.get(k) % j == 0 && j != nums.get(k)) {
+					if (nums.get(k) > Math.sqrt(nums.get(k))) {
+						compositeList.add(nums.get(k));
+						out.println(compositeList);
+					}
+				}
+			}
+		}
+		for (int l = 0; l < compositeList.size()-1; l++) {
+			if (!(nums.contains(compositeList.get(l)))) {
+				nums.remove()
+			}
+		}
+
 	}
 }
