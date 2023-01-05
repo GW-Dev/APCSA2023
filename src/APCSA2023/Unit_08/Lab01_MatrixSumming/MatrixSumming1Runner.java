43aa1547a31e218741bf51f0fe005d2e5e7c0da3
GW-Dev <grantewilliams51@gmail.com>
2023-01-05 09:24:35 -0600

    49

--- src/APCSA2023/Unit_08/Lab01_MatrixSumming/MatrixSumming1Runner.java
+++ src/APCSA2023/Unit_08/Lab01_MatrixSumming/MatrixSumming1Runner.java
@@ -17,10 +17,11 @@ public class MatrixSumming1Runner
 		int[][] m = MatrixSumming1.getArr();
 		String prettyMatrix = "";
 		int initRow = 0;
+		int rowLength = m[0].length;
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m[i].length; j++) {
 				prettyMatrix += m[i][j] + " ";
-				if (i > initRow) {
+				if (i == initRow || (i == 0 && (m[i].equals(rowLength)))) {
 					initRow++;
 					prettyMatrix += "\n";
 
