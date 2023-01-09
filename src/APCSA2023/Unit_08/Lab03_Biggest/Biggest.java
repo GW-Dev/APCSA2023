6ea8035433a703877026d4ad43f57137971842bc
GW-Dev <grantewilliams51@gmail.com>
2023-01-09 08:19:51 -0600

    65

--- src/APCSA2023/Unit_08/Lab03_Biggest/Biggest.java
+++ src/APCSA2023/Unit_08/Lab03_Biggest/Biggest.java
@@ -6,6 +6,14 @@ public class Biggest
 {
     public static int getBig(int[][] m)
     {
-		return -1;
+        int bigNum = m[0][0];
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                if (m[i][j] > bigNum) {
+                    bigNum = m[i][j];
+                }
+            }
+        }
+		return bigNum;
     }
 }
