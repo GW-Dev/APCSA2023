e30d577cdb53015e832dd2aa14a8686cda20ea3a
GW-Dev <grantewilliams51@gmail.com>
2023-01-09 08:37:38 -0600

    68

--- src/APCSA2023/Unit_08/Lab04_TotalRow/TotalRow.java
+++ src/APCSA2023/Unit_08/Lab04_TotalRow/TotalRow.java
@@ -8,7 +8,18 @@ public class TotalRow
 {
     public static List<Integer> getRowTotals( int[][] m )
     {
+        int rowSum = 0;
+        List<Integer> toReturn = new ArrayList<>();
     	//add code here
-		return null;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                rowSum += m[i][j];
+                if (m[i].length - 1== j) {
+                    toReturn.add(rowSum);
+                    rowSum = 0;
+                }
+            }
+        }
+		return toReturn;
     }
 }
--- src/APCSA2023/Unit_08/Lab04_TotalRow/TotalRowRunner.java
+++ src/APCSA2023/Unit_08/Lab04_TotalRow/TotalRowRunner.java
@@ -6,7 +6,13 @@ public class TotalRowRunner
 {
 	public static void main( String args[] ) throws Exception
 	{
-		//add code here			
+		//add code here
+		int[][] a = {{1,2,3},{5,5,5,5}};
+		int[][] b = {{1,2,3},{5},{1},{2,2}};
+		int[][] c = {{1,2},{5,5},{5,5},{4,5,6,7},{123124,12312}};
+		System.out.println("Row totals are :: " + TotalRow.getRowTotals(a));
+		System.out.println("Row totals are :: " + TotalRow.getRowTotals(b));
+		System.out.println("Row totals are :: " + TotalRow.getRowTotals(c));
 	}
 }
 
